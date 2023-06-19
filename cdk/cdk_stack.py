848307bb53975fdd654361cc62aa03e6ba7bbc4d
Jubril Adeyi <jadeyi@gmail.com>
2023-06-19 15:40:03 +0100

    Modified vpc: added private subnets, added igw and route table with association

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -41,8 +41,41 @@ class CdkAppStack(Stack):
         )
 
         # Create a security group
-        security_group = ec2.SecurityGroup(
+        security_group = ec2.CfnSecurityGroup(
             self, "MySecurityGroup",
-            vpc=self.vpc,
-            allow_all_outbound=True,
+            group_description="server security group",
+            vpc_id=self.vpc.vpc_id,
+            security_group_ingress=[
+                {
+                    "ipProtocol": "tcp",
+                    "fromPort": 80,
+                    "toPort": 80,
+                    "cidrIp": "0.0.0.0/0"
+                },
+                {
+                    "ipProtocol": "tcp",
+                    "fromPort": 443,
+                    "toPort": 443,
+                    "cidrIp": "0.0.0.0/0"
+                },
+                {
+                    "ipProtocol": "tcp",
+                    "fromPort": 22,
+                    "toPort": 22,
+                    "cidrIp": "0.0.0.0/0"
+                }
+            ],
+            security_group_egress=[
+                {
+                    "ipProtocol": "-1",
+                    "cidrIp": "0.0.0.0/0"
+                }
+            ]
         )
+
+        # # Create a security group
+        # security_group = ec2.SecurityGroup(
+        #     self, "MySecurityGroup",
+        #     vpc=self.vpc,
+        #     allow_all_outbound=True,
+        # )
