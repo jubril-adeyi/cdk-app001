6115a970ed778d277d8b8b5bba752e57a61a8293
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-20 00:25:46 +0100

    create bastion host and sg

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,10 +1,10 @@
 from constructs import Construct
 from aws_cdk import (Duration, 
     Stack, 
-    aws_ec2 as ec2, 
+    aws_ec2 as ec2, )
     # CfnOutput, 
     # autoscaling as autoscale, 
-    aws_elbv2 as elb)
+    # aws_elbv2 as elb)
 
 class CdkAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
