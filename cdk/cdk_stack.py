7ebedd8134b34db78ebd36e2d7dc79646119f348
Jubril Adeyi <jadeyi@gmail.com>
2023-06-19 14:05:57 +0100

    Modified vpc: added private subnets

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,52 +1,38 @@
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_ec2 as ec2,
-
-
-)
+from aws_cdk import Duration, Stack, aws_ec2 as ec2
 from constructs import Construct
 
 class CdkAppStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-    
-        subnet_mask= 24
-        vpc_cidr= "10.0.0.0/16"
 
+        subnet_mask = 24
+        vpc_cidr = "10.0.0.0/16"
 
         vpc = ec2.Vpc(
             self,
             "MyVpc",
-            vpc_name= "server-vpc",
+            vpc_name="server-vpc",
             cidr=vpc_cidr,
-
             subnet_configuration=[
                 ec2.SubnetConfiguration(
                     subnet_type=ec2.SubnetType.PUBLIC,
                     name="Public1",
                     cidr_mask=subnet_mask
                 ),
-
                 ec2.SubnetConfiguration(
                     subnet_type=ec2.SubnetType.PUBLIC,
                     name="Public2",
                     cidr_mask=subnet_mask
-                )
-
+                ),
                 ec2.SubnetConfiguration(
                     subnet_type=ec2.SubnetType.PRIVATE,
                     name="Private1",
                     cidr_mask=subnet_mask
                 ),
-
                 ec2.SubnetConfiguration(
                     subnet_type=ec2.SubnetType.PRIVATE,
                     name="Private2",
                     cidr_mask=subnet_mask
                 )
-            
             ]
-            
         )
