75a6ca9759eec7a1f1b6182b41d49407c3fb07c1
Jubril Adeyi <jadeyi@gmail.com>
2023-06-20 09:36:49 +0100

    Update LoadBalancer creation

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -196,29 +196,29 @@ class CdkAppStack(Stack):
         #     instance_type=ec2.InstanceType("t2.micro"),
         # )
 
-        # public_server_1 = ec2.Instance(
-        #     self,
-        #     "public-server-1",
-        #     instance_name="server03",
-        #     machine_image=ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
-        #     key_name=key_name,
-        #     vpc=self.vpc,
-        #     vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public1"),
-        #     security_group=self.server_security_group,
-        #     instance_type=ec2.InstanceType("t2.micro"), 
-        # )
+        public_server_1 = ec2.Instance(
+            self,
+            "public-server-1",
+            instance_name="server03",
+            machine_image=ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
+            key_name=key_name,
+            vpc=self.vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public1"),
+            security_group=self.server_security_group,
+            instance_type=ec2.InstanceType("t2.micro"), 
+        )
 
-        # public_server_2 = ec2.Instance(
-        #     self,
-        #     "public-server-2",
-        #     instance_name="server04",
-        #     machine_image=ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
-        #     key_name=key_name,
-        #     vpc=self.vpc,
-        #     vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public2"),
-        #     security_group=self.server_security_group,
-        #     instance_type=ec2.InstanceType("t2.micro"),
-        # )
+        public_server_2 = ec2.Instance(
+            self,
+            "public-server-2",
+            instance_name="server04",
+            machine_image=ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
+            key_name=key_name,
+            vpc=self.vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public1"),
+            security_group=self.server_security_group,
+            instance_type=ec2.InstanceType("t2.micro"),
+        )
 
 
 
@@ -256,7 +256,7 @@ class CdkAppStack(Stack):
         #     machine_image=ec2.MachineImage.latest_amazon_linux(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
         #     key_name=key_name,
         #     vpc=self.vpc,
-        #     vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public2"),
+        #     vpc_subnets=ec2.SubnetSelection(subnet_group_name="Public1"),
         #     security_group=self.bastion_sg,
         #     instance_type=ec2.InstanceType("t2.micro"),
         # )
@@ -304,7 +304,7 @@ class CdkAppStack(Stack):
             internet_facing=True,
             security_group=self.lb_security_group,
 
-            # vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
         )
 
 
