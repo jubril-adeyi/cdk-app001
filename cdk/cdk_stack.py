9701e7c9a1f70a6b43a86165f17105855f0dd882
Jubril Adeyi <jadeyi@gmail.com>
2023-06-20 13:08:23 +0100

    Update LoadBalancer creation

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -348,9 +348,6 @@ class CdkAppStack(Stack):
 
 
         # Extract public Ips into host inventory file 
-        public_server_1_ip = public_server_1.instance_public_ip
-        public_server_2_ip = public_server_2.instance_public_ip
-
         # create list
 
         ip_addresses = []
