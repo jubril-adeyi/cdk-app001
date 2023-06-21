bf24e482d7f9ae1c2872d13ba40a6c493e403c26
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-21 17:52:26 +0100

    modularizing cdk app

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from lib.vpc_module import VpcModule
 from lib.ec2_instance_module import Ec2Module
 # from lib.load_balancer_module import LoadbalancerModule
 
-Main appStack that calls other modules 
+# Main appStack that calls other modules 
 class CdkAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str,  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
