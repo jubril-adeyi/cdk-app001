9e842643126affdb668bd9c35276d5cf5d2ad8eb
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-21 11:09:38 +0100

    modularizing cdk app

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ import os
 
 import aws_cdk as cdk
 
-from stacks.cdk_stack import CdkAppStack
+# from lib.cdk_stack import CdkAppStack
+from lib.vpc_module import VpcModule
+
+# Main appStack that calls other modules 
+
+
+class CdkAppStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        vpc_module = VpcModule(self, "VpcModule")
 
 
 app = cdk.App()
---
+++ lib/vpc_module.py
@@ -0,0 +1,173 @@
+from constructs import Construct
+from aws_cdk import (Duration, 
+    Stack, 
+    aws_ec2 as ec2,
+    )
+
+class VpcModule(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        ## Create Vpc 
+
+        subnet_mask = 24
+        vpc_cidr = "10.0.0.0/16"
+
+        self.vpc = ec2.Vpc(
+            self,
+            "MyVpc",
+            vpc_name="server-vpc",
+            max_azs=2,
+            cidr=vpc_cidr,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    name="Public1",
+                    cidr_mask=subnet_mask
+                ),
+                # ec2.SubnetConfiguration(
+                #     subnet_type=ec2.SubnetType.PUBLIC,
+                #     name="Public2",
+                #     cidr_mask=subnet_mask
+                # ),
+                ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    name="Privatewithnat1",
+                    cidr_mask=subnet_mask
+                ),
+                # ec2.SubnetConfiguration(
+                #     subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                #     name="Privatewithnat2",
+                #     cidr_mask=subnet_mask
+                # )
+            ]
+        )
+
+        # Create a security group lb-sg
+        self.lb_security_group = ec2.SecurityGroup(
+            self, "LbSecurityGroup",
+            security_group_name="lb-sg",
+            description="lb security group",
+            vpc=self.vpc
+        )
+
+        self.lb_security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.tcp(80),
+        )
+
+        self.lb_security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.tcp(22),
+        )
+
+        self.lb_security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.tcp(443),
+        )      
+
+        self.lb_security_group.add_egress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.all_traffic(),
+        )       
+
+
+        # Create a security group server-sg
+
+
+        self.server_security_group = ec2.SecurityGroup(
+            self, "ServerSecurityGroup",
+            security_group_name="server-sg",
+            description="server security group",
+            vpc=self.vpc
+        )
+
+        self.server_security_group.add_ingress_rule(
+            peer=self.lb_security_group,
+            connection=ec2.Port.tcp(80),
+        )
+
+        # self.server_security_group.add_ingress_rule(
+        # peer=ec2.Peer.ipv4("0.0.0.0/0"),
+        # connection=ec2.Port.tcp(443),
+        # )   
+
+        self.server_security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.tcp(22),
+        )
+
+        self.server_security_group.add_egress_rule(
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            connection=ec2.Port.all_traffic(),
+        )
+
+        
+
+        # Create a security group server-sg using CfnSecurityGroup
+
+        # self.server_security_group = ec2.CfnSecurityGroup(
+        #     self, "ServerSecurityGroup",
+        #     group_description="server security group",
+        #     group_name="server-sg",
+        #     vpc_id=self.vpc.vpc_id,
+        #     security_group_ingress=[
+        #         {
+        #             "ipProtocol": "tcp",
+        #             "fromPort": 80,
+        #             "toPort": 80,
+        #             "cidrIp": "0.0.0.0/0"
+        #         },
+        #         {
+        #             "ipProtocol": "tcp",
+        #             "fromPort": 443,
+        #             "toPort": 443,
+        #             "cidrIp": "0.0.0.0/0"
+        #         },
+        #         {
+        #             "ipProtocol": "tcp",
+        #             "fromPort": 22,
+        #             "toPort": 22,
+        #             "cidrIp": "0.0.0.0/0"
+        #         }
+        #     ],
+        #     security_group_egress=[
+        #         {
+        #             "ipProtocol": "-1",
+        #             "cidrIp": "0.0.0.0/0"
+        #         }
+        #     ]
+        # )
+
+        # # Create a security group lb-sg using CfnSecurityGroup
+        
+        # self.lb_security_group = ec2.CfnSecurityGroup(
+        #     self, "LbSecurityGroup",
+        #     group_description="lb security group",
+        #     group_name="lb-sg",
+        #     vpc_id=self.vpc.vpc_id,
+        #     security_group_ingress=[
+        #         {
+        #             "ipProtocol": "tcp",
+        #             "fromPort": 80,
+        #             "toPort": 80,
+        #             "cidrIp": "0.0.0.0/0"
+        #         },
+        #         {
+        #             "ipProtocol": "tcp",
+        #             "fromPort": 22,
+        #             "toPort": 22,
+        #             "cidrIp": "0.0.0.0/0"
+        #         }
+        #     ],
+        #     security_group_egress=[
+        #         {
+        #             "ipProtocol": "-1",
+        #             "cidrIp": "0.0.0.0/0"
+        #         }
+        #     ]
+        # )
+
+        
+
+       
