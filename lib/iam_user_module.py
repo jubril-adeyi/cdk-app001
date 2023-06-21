b5e8e29ebabe7b5c8bd258c31d503f171ced2f15
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-21 20:49:50 +0100

    modularizing cdk app

--- lib/iam_user_module.py
+++ lib/iam_user_module.py
@@ -1,7 +1,7 @@
 from aws_cdk import Stack
 from aws_cdk import aws_iam as iam
 
-class IamUserStack(cdk.Stack):
+class IamUserStack(Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
