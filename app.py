746da952d1f0c7e3dfaff5990ef6af2f181bae80
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-21 18:29:05 +0100

    modularizing cdk app

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 import os
 import aws_cdk as cdk
-from aws_cdk import Stack
-from constructs import Construct
+# from aws_cdk import Stack
+# from constructs import Construct
 
 # from lib import neccesary modules
 from lib.vpc_module import VpcModule
-# from lib.ec2_instance_module import Ec2Module
+from lib.ec2_instance_module import Ec2Module
 # from lib.load_balancer_module import LoadbalancerModule
 
 # Main appStack that calls other modules 
 
-class CdkAppStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str,  **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+# class CdkAppStack(Stack):
+#     def __init__(self, scope: Construct, construct_id: str,  **kwargs) -> None:
+#         super().__init__(scope, construct_id, **kwargs)
 
-        vpc_module = VpcModule(self, "VpcModule")
+#         vpc_module = VpcModule(self, "VpcModule")
 
-        # ec2_instance_module=Ec2Module(self,"Ec2Module")
+#         # ec2_instance_module=Ec2Module(self,"Ec2Module")
 
-        # load_balancer_module=LoadbalancerModule(self,"LbModule", ec2_instance_module.ec2, vpc_module.vpc)
+#         # load_balancer_module=LoadbalancerModule(self,"LbModule", ec2_instance_module.ec2, vpc_module.vpc)
+
+# app = cdk.App()
+# CdkAppStack(app, "CdkAppStack")
+
+# app.synth()
 
 app = cdk.App()
-CdkAppStack(app, "CdkAppStack")
+
+# Instantiate VpcModule
+vpc_module = VpcModule(app, "VpcModule")
+
+# # Instantiate Ec2Module and pass the VPC and server security group
+# ec2_module = Ec2Module(app, "Ec2Module", vpc=vpc_module.vpc, server_security_group=vpc_module.server_security_group)
+
+# # Add more modules/stacks as needed
+
+# # Add stack dependencies if necessary
+# ec2_module.add_dependency(vpc_module)
 
 app.synth()
