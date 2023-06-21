baa78a8bf5c059db0298ce8bdc44ca81775def4a
gbolahan adeyi <jbadeyi@icloud.com>
2023-06-21 11:13:43 +0100

    modularizing cdk app

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
 from aws_cdk import Stack
+from constructs import Construct
 
 # from lib.cdk_stack import CdkAppStack
 from lib.vpc_module import VpcModule
 
 # Main appStack that calls other modules 
-
-
 class CdkAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         vpc_module = VpcModule(self, "VpcModule")
+
+        
 
 
 app = cdk.App()
